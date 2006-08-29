66605f2a9c8ef149545f12746e1c7a48a2c33624
ShadoWarrior <ShadoWarrior@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2006-08-29 21:41:58 +0000

    Create a new branch
    git-svn-id: https://ja2svn.mooo.com/source/ja2@495 3b4a5df2-a311-0410-b5c6-a8a6f20db521

---
+++ branches/ShadoWarrior/ja2_v1.13/Build/Laptop/BobbyRGuns.h
@@ -0,0 +1,80 @@
+#ifndef __BOBBYRGUNS_H
+#define __BOBBYRGUNS_H
+
+//#include "LaptopSave.h"
+
+
+#define		BOBBYRDESCFILE										"BINARYDATA\\BrayDesc.edt"
+
+#define		BOBBYR_ITEM_DESC_NAME_SIZE				160
+#define		BOBBYR_ITEM_DESC_INFO_SIZE				640
+#define		BOBBYR_ITEM_DESC_FILE_SIZE				800
+
+#define		BOBBYR_USED_ITEMS			0xFFFFFFFF
+
+
+#define		BOBBYR_GUNS_BUTTON_FONT						FONT10ARIAL
+#define		BOBBYR_GUNS_TEXT_COLOR_ON					FONT_NEARBLACK // FONT_DKGREEN
+#define		BOBBYR_GUNS_TEXT_COLOR_OFF				FONT_NEARBLACK
+//#define		BOBBYR_GUNS_TEXT_COLOR_ON					FONT_MCOLOR_DKWHITE
+//#define		BOBBYR_GUNS_TEXT_COLOR_OFF				FONT_MCOLOR_WHITE
+
+#define		BOBBYR_GUNS_SHADOW_COLOR					169
+
+#define		BOBBYR_NO_ITEMS										65535
+
+
+
+
+
+extern UINT16			gusCurWeaponIndex;
+extern UINT8			gubLastGunIndex;
+
+
+
+void GameInitBobbyRGuns();
+BOOLEAN EnterBobbyRGuns();
+void ExitBobbyRGuns();
+void HandleBobbyRGuns();
+void RenderBobbyRGuns();
+
+// WANNE
+BOOLEAN DeleteBobbyRGunsFilter();
+BOOLEAN DeleteBobbyRAmmoFilter();
+BOOLEAN DeleteBobbyRUsedFilter();
+BOOLEAN InitBobbyRGunsFilterBar();
+BOOLEAN InitBobbyRAmmoFilterBar();
+BOOLEAN InitBobbyRUsedFilterBar();
+
+BOOLEAN DisplayBobbyRBrTitle();
+BOOLEAN DeleteBobbyBrTitle();
+BOOLEAN InitBobbyBrTitle();
+BOOLEAN InitBobbyMenuBar( );
+BOOLEAN DeleteBobbyMenuBar();
+
+
+
+//BOOLEAN DisplayWeaponInfo();
+BOOLEAN DisplayItemInfo(UINT32 uiItemClass, INT32 iFilter);
+void DeleteMouseRegionForBigImage();
+void UpdateButtonText(UINT32	uiCurPage);
+
+// WANNE
+void UpdateAmmoFilterButtons(INT32 iNewButton, INT32 iOldButton);
+void UpdateGunFilterButtons(INT32 iNewButton, INT32 iOldButton);
+void UpdateUsedFilterButtons();
+
+void EnterInitBobbyRGuns();
+void DailyUpdateOfBobbyRaysUsedInventory();
+UINT16 CalcBobbyRayCost( UINT16 usIndex, UINT16 usBobbyIndex, BOOLEAN fUsed);
+//void CalculateFirstAndLastIndexs();
+void SetFirstLastPagesForUsed(INT32 iFilter);
+void SetFirstLastPagesForNew( UINT32 uiClass, INT32 iFilter );
+
+#endif
+
+
+
+ 
+
+
