9dcf9632f4a06c9f36933083a6a5ecccdcbfb755
Sergeant_Kolja <Sergeant_Kolja@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-02-24 13:26:38 +0000

    modified make_help.bat in a way, all other users can invoke it too.
    git-svn-id: https://ja2svn.mooo.com/source/ja2@738 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- branches/Sergeant_Kolja/trunk/VSSC_for_JA2_113/make_help.bat
+++ branches/Sergeant_Kolja/trunk/VSSC_for_JA2_113/make_help.bat
@@ -1,8 +1,27 @@
 @echo off
-if     %doxypath%*==* set doxy_invoke=doxygen
-if not %doxypath%*==* set doxy_invoke=%doxypath%\doxygen
+
+rem Trying to find DOXYGEN TOOL, attempt #1:
+if exist %doxypath%\doxygen.exe goto add_doxy
+
+rem Trying to find DOXYGEN TOOL, attempt #2, look inside PATH variable:
+for %%I in (doxygen.exe) do set doxy_invoke=%%~$PATH:I
+if NOT "%doxy_invoke%"=="" goto set_dox
+
+rem Trying to find DOXYGEN TOOL, attempt #3: Doxy is NOT in PATH, try PrFiles -> doxypath
+if exist %ProgramFiles%\doxygen\bin\doxygen.exe set doxypath=%ProgramFiles%\doxygen\bin
+if exist %doxypath%\doxygen.exe goto add_doxy
+
+rem Trying to find DOXYGEN TOOL, attempt #34: no further idea :-(
+rem lets try to scan different user specific paths
+if exist d:\C\Versionsverwaltung\doxygen\bin\doxygen.exe set doxypath=d:\C\Versionsverwaltung\doxygen\bin
+if exist C:\doxygen\bin\doxygen.exe set doxypath=C:\doxygen\bin
+
+:add_doxy
+set doxy_invoke=%doxypath%\doxygen.exe
+echo --- Using '%doxy_invoke%'
 
 
+:set_dox
 for /R %%i in (*.doxyfile) do (
 	pushd %%~pi
 	echo calling %doxy_invoke% %%~nxi ...
