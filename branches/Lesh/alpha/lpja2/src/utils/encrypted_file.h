38c050e00e1e8a244a1e4a6f77da228e205b5720
Lesh <Lesh@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-07-28 18:02:45 +0000

    deletinc src - replacing
    git-svn-id: https://ja2svn.mooo.com/source/ja2@1131 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- branches/Lesh/alpha/lpja2/src/utils/encrypted_file.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ENCRYPTED_H_
-#define __ENCRYPTED_H_
-
-#include "types.h"
-
-BOOLEAN LoadEncryptedDataFromFile16(STR pFileName, void *pDestString, UINT32 uiSeekFrom, UINT32 uiSeekAmount);
-BOOLEAN LoadEncryptedDataFromFile(STR pFileName, STR16 pDestString, UINT32 uiSeekFrom, UINT32 uiSeekAmount);
-
-#endif
