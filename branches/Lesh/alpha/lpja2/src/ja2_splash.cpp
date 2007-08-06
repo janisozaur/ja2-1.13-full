675996c2a264a34f2fed929b3d213cc057ca3dc3
Lesh <Lesh@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-08-06 17:56:55 +0000

    updating alpha release
    git-svn-id: https://ja2svn.mooo.com/source/ja2@1167 3b4a5df2-a311-0410-b5c6-a8a6f20db521

---
+++ branches/Lesh/alpha/lpja2/src/ja2_splash.cpp
@@ -0,0 +1,98 @@
+#ifdef PRECOMPILEDHEADERS
+	#include "ja2_all.h"
+#else
+	//add includes here!
+	#include "types.h"
+	#include "ja2_splash.h"
+	#include "vsurface.h"
+	#include "video.h"
+	#include "vobject.h"
+	#include "utilities.h"
+	#include "timer_control.h"
+	#include "platform.h"
+	
+#endif
+
+UINT32 guiSplashFrameFade = 10;
+UINT32 guiSplashStartTime = 0;
+extern HVSURFACE ghFrameBuffer;
+extern BOOLEAN Unpack8BPPDataTo8BPPBuffer( UINT16 *pBuffer, UINT32 uiDestPitchBYTES, HVOBJECT hSrcVObject, INT32 iX, INT32 iY, UINT16 usIndex );
+extern SGPRect	ClippingRect;
+
+//Simply create videosurface, load image, and draw it to the screen.
+void InitJA2SplashScreen()
+{
+//#ifdef ENGLISH
+	//ClearMainMenu();
+
+//#else
+	UINT32 uiLogoID = 0, uiPic = 0;
+	HVSURFACE hVSurface; // unused jonathanl  // lalien reenabled for international versions
+	VSURFACE_DESC VSurfaceDesc; //unused jonathanl // lalien reenabled for international versions
+	HVOBJECT hHandle;
+	VOBJECT_DESC    VObjectDesc; 
+
+// #ifdef JA2TESTVERSION
+//	INT32 i = 0;
+//
+//	memset( &VSurfaceDesc, 0, sizeof( VSURFACE_DESC ) );
+//	VSurfaceDesc.fCreateFlags = VSURFACE_CREATE_FROMFILE | VSURFACE_SYSTEM_MEM_USAGE;
+//	sprintf( VSurfaceDesc.ImageFile, "LOADSCREENS\\Notification.sti" );
+//	if( !AddVideoSurface( &VSurfaceDesc, &uiLogoID ) )
+//	{	
+//		//AssertMsg( 0, String( "Failed to load %s", VSurfaceDesc.ImageFile ) );
+//		return;
+//	}
+//	GetVideoSurface(&hVSurface, uiLogoID );
+//	BltVideoSurfaceToVideoSurface( ghFrameBuffer, hVSurface, 0, 0, 0, 0, NULL );
+//	DeleteVideoSurfaceFromIndex( uiLogoID );
+//
+//
+//	InvalidateScreen();
+//	RefreshScreen( NULL );
+//
+//	guiSplashStartTime = GetJA2Clock();
+//	while( i < 60 * 15 )//guiSplashStartTime + 15000 > GetJA2Clock() )
+//	{
+//		//Allow the user to pick his bum.
+//		InvalidateScreen();
+//		RefreshScreen( NULL );
+//		i++;
+//	}
+//#endif
+	ClippingRect.iLeft		= 0;
+	ClippingRect.iTop		= 0;
+	ClippingRect.iRight		= SCREEN_WIDTH;
+	ClippingRect.iBottom	= SCREEN_HEIGHT;
+
+			memset( &VSurfaceDesc, 0, sizeof( VSURFACE_DESC ) );
+			VSurfaceDesc.fCreateFlags = VSURFACE_CREATE_FROMFILE | VSURFACE_SYSTEM_MEM_USAGE;
+			//GetMLGFilename( VSurfaceDesc.ImageFile, MLG_SPLASH );
+			sprintf( VSurfaceDesc.ImageFile, "LOADSCREENS\\splash.sti" );
+			if( !AddVideoSurface( &VSurfaceDesc, &uiLogoID ) )
+			{	
+				fprintf(stderr,	"Failed to load %s\n", VSurfaceDesc.ImageFile);
+				AssertMsg( 0, String( "Failed to load %s", VSurfaceDesc.ImageFile ) );
+				return;
+			}
+
+			GetVideoSurface( &hVSurface, uiLogoID );
+			BltVideoSurfaceToVideoSurface( ghFrameBuffer, hVSurface, 0, iScreenWidthOffset, iScreenHeightOffset, 0, NULL );
+			DeleteVideoSurfaceFromIndex( uiLogoID );
+	//#endif
+			VObjectDesc.fCreateFlags=VOBJECT_CREATE_FROMFILE;
+			FilenameForBPP( "INTERFACE\\MBS.sti", VObjectDesc.ImageFile );
+			if ( !AddVideoObject( &VObjectDesc, &uiPic ) )
+			{
+				fprintf(stderr,	"Failed to load %s\n", VObjectDesc.ImageFile);
+				AssertMsg( 0, String( "Failed to load %s", VObjectDesc.ImageFile ) );
+				return;
+			}
+			GetVideoObject(&hHandle, uiPic ); 
+			BltVideoObject( FRAME_BUFFER , hHandle, 0, 0, 0, VO_BLT_SRCTRANSPARENCY,NULL );
+
+	InvalidateScreen();
+	RefreshScreen( NULL );
+
+	guiSplashStartTime = GetJA2Clock();
+}
---
+++ branches/Lesh/alpha/lpja2/src/sgp/ja2_sgp_all.h
@@ -0,0 +1,91 @@
+/*
+ * ChangeLog:
+ *  10.12.2005 Lesh ripped out everything that refers to MSS
+ *  10.12.2005 Lesh added fmod.h
+ */ 
+#ifndef __JA2_SGP_ALL_H
+#define __JA2_SGP_ALL_H
+
+#pragma message("GENERATED PCH FOR JA2 SGP PROJECT.")
+
+//#ifndef INITGUID
+//	#define INITGUID
+//#endif
+
+
+#include "word_wrap.h"
+#include "video.h"
+#include "button_sound_control.h"
+#include "sound_control.h"
+#ifdef _JA2_RENDER_DIRTY
+	#include "font_control.h"
+	#include "render_dirty.h"
+	#include "utilities.h"
+#endif
+#include "input.h"
+#include "memman.h"
+#include "english.h"
+#include "vobject.h"
+#include "vobject_blitters.h"
+#include "sound_man.h"
+#include "button_system.h"
+#include "line.h"
+#include <stdarg.h>
+#include "debug.h"
+#include "types.h"
+#include <stdlib.h>
+#include <malloc.h>
+#include <stdio.h>
+#include "container.h"
+#if _MSC_VER < 1300 //(iostream.h was removed from VC.NET2003)
+#include <iostream.h>
+#endif
+#include "cursor_control.h"
+#include "wcheck.h"
+#include "file_man.h"
+#include "db_man.h"
+#include "topic_ids.h"
+#include "topic_ops.h"
+#include "screen_ids.h"
+#include "sys_globals.h"
+#include "ja_screens.h"
+#include "game_loop.h"
+#include <direct.h>
+#include "library_database.h"
+#include "io.h"
+#include <wchar.h>
+#include "sgp.h"
+#include "pcx.h"
+#include "font.h"
+#include "image.h"
+#include <math.h>
+#include <string.h>
+#include "imp_tga.h"
+#include "stci.h"
+#include <memory.h>
+#include "local.h"
+#include <tchar.h>
+#include <assert.h>
+#include	"game_settings.h"
+#ifdef _DEBUG
+	#include <crtdbg.h>
+#endif
+#include "mouse_system.h"
+#include "Random.h"
+#include "vobject_private.h"
+#include "shading.h"
+#include "img_fmt.h"
+#include "render_world.h"
+#include "Isometric utils.h"
+#include "fade screen.h"
+#include "timer control.h"
+#include "vsurface.h"
+#include "vsurface_private.h"
+#include "timer_control.h"
+
+// Lesh modifications
+#include "fmod.h"
+#include "fmod_errors.h"
+#include "SDL.h"
+
+#endif
