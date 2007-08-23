b9760829bd620390733078afcbd614b41d048f42
Lesh <Lesh@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-08-23 17:52:29 +0000

    updating alpha release
    git-svn-id: https://ja2svn.mooo.com/source/ja2@1270 3b4a5df2-a311-0410-b5c6-a8a6f20db521

---
+++ branches/Lesh/alpha/lpja2/src/laptop/imp_about_us.cpp
@@ -0,0 +1,133 @@
+#ifdef PRECOMPILEDHEADERS
+	#include "laptop_all.h"
+#else
+	#include "imp_about_us.h"
+	#include "char_profile.h"
+	#include "imp_video_objects.h"
+	#include "utilities.h"
+	#include "wcheck.h"
+	#include "debug.h"
+	#include "word_wrap.h"
+	#include "render_dirty.h"
+	#include "encrypted_file.h"
+	#include "cursors.h"
+	#include "laptop.h"
+	#include "imp_text_system.h"
+#endif
+
+// IMP AboutUs buttons
+INT32 giIMPAboutUsButton[1];
+INT32 giIMPAboutUsButtonImage[1];
+void CreateIMPAboutUsButtons( void );
+void DeleteIMPAboutUsButtons( void );;
+
+// fucntions
+void BtnIMPBackCallback(GUI_BUTTON *btn,INT32 reason);
+
+
+void EnterIMPAboutUs( void )
+{
+
+	// create buttons
+	CreateIMPAboutUsButtons( );
+
+	// entry into IMP about us page 
+	RenderIMPAboutUs( );
+	
+	return;
+}
+
+
+void ExitIMPAboutUs( void )
+{
+  // exit from IMP About us page
+  
+	// delete Buttons
+	DeleteIMPAboutUsButtons( ); 
+
+	return;
+}
+
+
+void RenderIMPAboutUs( void )
+{
+  // rneders the IMP about us page
+	
+	// the background
+	RenderProfileBackGround( );
+  
+	// the IMP symbol
+	RenderIMPSymbol( 106, 1 );
+
+	// about us indent
+	RenderAboutUsIndentFrame( 8, 130 );
+  // about us indent
+	RenderAboutUsIndentFrame( 258, 130 );
+
+	return;
+}
+
+
+void HandleIMPAboutUs( void )
+{
+  // handles the IMP about us page
+
+	return;
+}
+
+
+void CreateIMPAboutUsButtons( void )
+{
+
+  // this function will create the buttons needed for th IMP about us page
+  // the back button button
+  giIMPAboutUsButtonImage[0]=  LoadButtonImage( "LAPTOP\\button_3.sti" ,-1,0,-1,1,-1 );
+	/*giIMPAboutUsButton[0] = QuickCreateButton( giIMPAboutUsButtonImage[0], LAPTOP_SCREEN_UL_X +  426 , LAPTOP_SCREEN_WEB_UL_Y + ( 360 ),
+										BUTTON_TOGGLE, MSYS_PRIORITY_HIGHEST - 1,
+										BtnGenericMouseMoveButtonCallback, (GUI_CALLBACK)BtnIMPBackCallback); */
+
+   giIMPAboutUsButton[0] = CreateIconAndTextButton( giIMPAboutUsButtonImage[0], pImpButtonText[6], FONT12ARIAL, 
+														 FONT_WHITE, DEFAULT_SHADOW, 
+														 FONT_WHITE, DEFAULT_SHADOW, 
+														 TEXT_CJUSTIFIED, 
+														 LAPTOP_SCREEN_UL_X +  216, LAPTOP_SCREEN_WEB_UL_Y + ( 360 ), BUTTON_TOGGLE, MSYS_PRIORITY_HIGH,
+														 	BtnGenericMouseMoveButtonCallback, (GUI_CALLBACK)BtnIMPBackCallback);
+	 
+	 SetButtonCursor(giIMPAboutUsButton[0], CURSOR_WWW);
+	 
+	 return;
+}
+
+
+void DeleteIMPAboutUsButtons( void )
+{
+  // this function destroys the buttons needed for the IMP about Us Page
+
+  // the about back button
+  RemoveButton(giIMPAboutUsButton[0] );
+  UnloadButtonImage(giIMPAboutUsButtonImage[0] );
+
+
+	return;
+}
+
+void BtnIMPBackCallback(GUI_BUTTON *btn,INT32 reason)
+{
+
+	// btn callback for IMP Homepage About US button
+	if (!(btn->uiFlags & BUTTON_ENABLED))
+		return;
+
+	if(reason & MSYS_CALLBACK_REASON_LBUTTON_DWN )
+	{
+		 btn->uiFlags|=(BUTTON_CLICKED_ON);
+	}
+	else if(reason & MSYS_CALLBACK_REASON_LBUTTON_UP )
+	{
+		if (btn->uiFlags & BUTTON_CLICKED_ON)
+		{
+      btn->uiFlags&=~(BUTTON_CLICKED_ON);
+      iCurrentImpPage = IMP_HOME_PAGE;
+		}
+	}	
+} 
---
+++ branches/Lesh/alpha/lpja2/src/tile_engine/world_dat.cpp
@@ -0,0 +1,233 @@
+#ifdef PRECOMPILEDHEADERS
+	#include "tile_engine_all.h"
+#else
+	#include "platform.h"
+	#include "world_dat.h"
+	#include "world_def.h"
+	#include "tile_def.h"
+	#include "sys_globals.h"
+	#include "tile_surface.h"
+	#include "file_man.h"
+	#include "sgp_str.h"
+#endif
+
+// THIS FILE CONTAINS DEFINITIONS FOR TILESET FILES
+
+void SetTilesetThreeTerrainValues();
+void SetTilesetFourTerrainValues();
+
+// Snap: This global gives the number of tilesets in JA2SET.DAT
+// It is properly initialized in InitEngineTilesets,
+// where it is read from JA2SET.DAT header.
+UINT8 gubNumSets = MAX_TILESETS;
+
+TILESET	 gTilesets[ MAX_TILESETS ];
+
+
+void InitEngineTilesets( )
+{
+	UINT32		cnt, cnt2, uiNumFiles;
+//	FILE					*hfile;
+	HWFILE			hfile;
+	CHAR8			zName[32];
+	UINT32		uiNumBytesRead;
+
+	//OPEN FILE
+//	hfile = fopen( "BINARYDATA\\JA2SET.DAT", "rb" );
+	hfile = FileOpen( "BINARYDATA\\JA2SET.DAT", FILE_ACCESS_READ, FALSE );
+	if ( !hfile  )
+	{
+		SET_ERROR( "Cannot open tileset data file" );
+		return;
+	}
+
+		
+	// READ # TILESETS and compare
+//	fread( &gubNumSets, sizeof( gubNumSets ), 1, hfile );
+	FileRead( hfile, &gubNumSets, sizeof( gubNumSets ), &uiNumBytesRead );
+	// CHECK
+	if ( gubNumSets > MAX_TILESETS )
+	{
+		// Report error
+		SET_ERROR( "Too many tilesets in the data file" );
+		return;
+	}
+
+	// READ #files
+//	fread( &uiNumFiles, sizeof( uiNumFiles ), 1, hfile );
+	FileRead( hfile, &uiNumFiles, sizeof( uiNumFiles ), &uiNumBytesRead );
+
+	// COMPARE
+	if ( uiNumFiles != NUMBEROFTILETYPES )
+	{
+		// Report error
+		SET_ERROR( "Number of tilesets slots in code does not match data file" );
+		return;
+	}
+
+
+	// Loop through each tileset, load name then files
+	for ( cnt = 0; cnt < gubNumSets; cnt++ )
+	{
+		//Read name
+//		fread( &zName, sizeof( zName ), 1, hfile );
+		FileRead( hfile, &zName, sizeof( zName ), &uiNumBytesRead );
+
+		// Read ambience value
+//		fread( &(gTilesets[ cnt ].ubAmbientID), sizeof( UINT8), 1, hfile );
+		FileRead( hfile, &(gTilesets[ cnt ].ubAmbientID), sizeof( UINT8 ), &uiNumBytesRead );
+
+		// Set into tileset 
+		WSTR_SPrintf( (wchar_t *)gTilesets[ cnt ].zName, 32, L"%S", zName );
+
+		// Loop for files
+		for ( cnt2 = 0; cnt2 < uiNumFiles; cnt2++ )
+		{
+			// Read file name
+//			fread( &zName, sizeof( zName ), 1, hfile );
+			FileRead( hfile, &zName, sizeof( zName ), &uiNumBytesRead );
+
+			// Set into database
+			strcpy( gTilesets[ cnt ].TileSurfaceFilenames[ cnt2 ], zName );
+
+		}
+
+	}
+
+//	fclose( hfile );
+	FileClose( hfile );
+
+
+	// SET CALLBACK FUNTIONS!!!!!!!!!!!!!
+	gTilesets[ TLS_CAVES_1 ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetTwoTerrainValues;
+	gTilesets[ TLS_AIRSTRIP ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+	gTilesets[ TLS_DEAD_AIRSTRIP ].MovementCostFnc	= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+	gTilesets[ TLS_PALACE ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+	gTilesets[ TLS_BALIME ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+	gTilesets[ TLS_BALIME_MUSEUM ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+	gTilesets[ TLS_QUEENS_TROPICAL ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+	gTilesets[ TLS_MEDUNA_INNER_TOWN ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+	gTilesets[ TLS_QUEENS_SAM ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+	gTilesets[ TLS_QUEENS_AIRPORT ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetThreeTerrainValues;
+
+	gTilesets[ TLS_TROPICAL_1 ].MovementCostFnc			= (TILESET_CALLBACK)SetTilesetFourTerrainValues;
+	gTilesets[ TLS_DESERT_SAM ].MovementCostFnc				= (TILESET_CALLBACK)SetTilesetFourTerrainValues;
+}
+
+
+
+
+void SetTilesetOneTerrainValues( )
+{
+		// FIRST TEXUTRES
+		gTileSurfaceArray[ FIRSTTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ SECONDTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ THIRDTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FOURTHTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FIFTHTEXTURE ]->ubTerrainID = LOW_GRASS;
+		gTileSurfaceArray[ SIXTHTEXTURE ]->ubTerrainID = LOW_GRASS;
+		gTileSurfaceArray[ SEVENTHTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ REGWATERTEXTURE ]->ubTerrainID = LOW_WATER;
+		gTileSurfaceArray[ DEEPWATERTEXTURE ]->ubTerrainID = DEEP_WATER;
+
+		// NOW ROADS
+		gTileSurfaceArray[ FIRSTROAD ]->ubTerrainID = DIRT_ROAD;
+		gTileSurfaceArray[ ROADPIECES ]->ubTerrainID = DIRT_ROAD;
+
+		// NOW FLOORS
+		gTileSurfaceArray[ FIRSTFLOOR ]->ubTerrainID = FLAT_FLOOR;
+		gTileSurfaceArray[ SECONDFLOOR ]->ubTerrainID = FLAT_FLOOR;
+		gTileSurfaceArray[ THIRDFLOOR ]->ubTerrainID = FLAT_FLOOR;
+		gTileSurfaceArray[ FOURTHFLOOR ]->ubTerrainID = FLAT_FLOOR;
+
+		// NOW ANY TERRAIN MODIFYING DEBRIS
+
+}
+
+
+void SetTilesetTwoTerrainValues( )
+{
+		// FIRST TEXUTRES
+		gTileSurfaceArray[ FIRSTTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ SECONDTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ THIRDTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FOURTHTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FIFTHTEXTURE ]->ubTerrainID = LOW_GRASS;
+		gTileSurfaceArray[ SIXTHTEXTURE ]->ubTerrainID = LOW_GRASS;
+		gTileSurfaceArray[ SEVENTHTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ REGWATERTEXTURE ]->ubTerrainID = LOW_WATER;
+		gTileSurfaceArray[ DEEPWATERTEXTURE ]->ubTerrainID = DEEP_WATER;
+
+		// NOW ROADS
+		gTileSurfaceArray[ FIRSTROAD ]->ubTerrainID = DIRT_ROAD;
+		gTileSurfaceArray[ ROADPIECES ]->ubTerrainID = DIRT_ROAD;
+
+
+		// NOW FLOORS
+		gTileSurfaceArray[ FIRSTFLOOR ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ SECONDFLOOR ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ THIRDFLOOR ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FOURTHFLOOR ]->ubTerrainID = FLAT_GROUND;
+
+}
+
+
+void SetTilesetThreeTerrainValues( )
+{
+		// DIFFERENCE FROM #1 IS THAT ROADS ARE PAVED
+
+		// FIRST TEXUTRES
+		gTileSurfaceArray[ FIRSTTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ SECONDTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ THIRDTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FOURTHTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FIFTHTEXTURE ]->ubTerrainID = LOW_GRASS;
+		gTileSurfaceArray[ SIXTHTEXTURE ]->ubTerrainID = LOW_GRASS;
+		gTileSurfaceArray[ SEVENTHTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ REGWATERTEXTURE ]->ubTerrainID = LOW_WATER;
+		gTileSurfaceArray[ DEEPWATERTEXTURE ]->ubTerrainID = DEEP_WATER;
+
+		// NOW ROADS
+		gTileSurfaceArray[ FIRSTROAD ]->ubTerrainID = PAVED_ROAD;
+		gTileSurfaceArray[ ROADPIECES ]->ubTerrainID = PAVED_ROAD;
+
+
+		// NOW FLOORS
+		gTileSurfaceArray[ FIRSTFLOOR ]->ubTerrainID = FLAT_FLOOR;
+		gTileSurfaceArray[ SECONDFLOOR ]->ubTerrainID = FLAT_FLOOR;
+		gTileSurfaceArray[ THIRDFLOOR ]->ubTerrainID = FLAT_FLOOR;
+		gTileSurfaceArray[ FOURTHFLOOR ]->ubTerrainID = FLAT_FLOOR;
+
+		// NOW ANY TERRAIN MODIFYING DEBRIS
+
+}
+
+void SetTilesetFourTerrainValues( )
+{
+		// DIFFERENCE FROM #1 IS THAT FLOOR2 IS NOT FLAT_FLOOR BUT FLAT_GROUND
+
+		// FIRST TEXUTRES
+		gTileSurfaceArray[ FIRSTTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ SECONDTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ THIRDTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FOURTHTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ FIFTHTEXTURE ]->ubTerrainID = LOW_GRASS;
+		gTileSurfaceArray[ SIXTHTEXTURE ]->ubTerrainID = LOW_GRASS;
+		gTileSurfaceArray[ SEVENTHTEXTURE ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ REGWATERTEXTURE ]->ubTerrainID = LOW_WATER;
+		gTileSurfaceArray[ DEEPWATERTEXTURE ]->ubTerrainID = DEEP_WATER;
+
+		// NOW ROADS
+		gTileSurfaceArray[ FIRSTROAD ]->ubTerrainID = DIRT_ROAD;
+		gTileSurfaceArray[ ROADPIECES ]->ubTerrainID = DIRT_ROAD;
+
+
+		// NOW FLOORS
+		gTileSurfaceArray[ FIRSTFLOOR ]->ubTerrainID = FLAT_FLOOR;
+		gTileSurfaceArray[ SECONDFLOOR ]->ubTerrainID = FLAT_GROUND;
+		gTileSurfaceArray[ THIRDFLOOR ]->ubTerrainID = FLAT_FLOOR;
+		gTileSurfaceArray[ FOURTHFLOOR ]->ubTerrainID = FLAT_FLOOR;
+
+		// NOW ANY TERRAIN MODIFYING DEBRIS
+
+}
