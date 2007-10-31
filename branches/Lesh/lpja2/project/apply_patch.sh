3bcc935dbf20cf4d7cecbdacbeda0689fbdc5fc6
Lesh <Lesh@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2007-10-31 18:05:45 +0000

    merged rev.922 from main trunk, updated apply_patch.sh
    git-svn-id: https://ja2svn.mooo.com/source/ja2@1564 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- branches/Lesh/lpja2/project/apply_patch.sh
+++ branches/Lesh/lpja2/project/apply_patch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-patch --directory=/home/lesh/lpja2 --input=$1 --posix -p0 --no-backup-if-mismatch $2
+patch --directory=/home/lesh/projects/lpja2 --input=$1 --posix -p0 --no-backup-if-mismatch $2
