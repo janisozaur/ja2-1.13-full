263c9072fa3f11c780a1e6250e4390107289850e
ChrisL <ChrisL@3b4a5df2-a311-0410-b5c6-a8a6f20db521>
2008-01-29 18:07:21 +0000

    git-svn-id: https://ja2svn.mooo.com/source/ja2@1775 3b4a5df2-a311-0410-b5c6-a8a6f20db521

--- branches/ChrisL/XMLEditor/GUI/Forms/LoadingForm.vb
+++
@@ -1,7 +0,0 @@
-Public Class LoadingForm
-
-    Public Shadows Sub Show(Optional ByVal saving As Boolean = False)
-        If saving Then InfoLabel.Text = "Saving..."
-        MyBase.Show()
-    End Sub
-End Class
